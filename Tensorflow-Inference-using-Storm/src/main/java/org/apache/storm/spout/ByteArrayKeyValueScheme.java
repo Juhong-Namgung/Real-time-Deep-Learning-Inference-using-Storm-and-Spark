9b8abb12f75a93dc50f49ca2281858bb02058521
Juhong Namgung <wnghd9999@naver.com>
2019-10-31 22:59:22 +0900

    Add ByteArrayKeyValueScheme to get data from Kafka(KeyValueScheme 구현)

---
+++ Tensorflow-Inference-using-Storm/src/main/java/org/apache/storm/spout/ByteArrayKeyValueScheme.java
@@ -0,0 +1,22 @@
+package org.apache.storm.spout;
+
+import com.google.common.collect.ImmutableMap;
+import org.apache.storm.kafka.KeyValueScheme;
+import org.apache.storm.spout.RawScheme;
+import org.apache.storm.tuple.Values;
+
+import java.nio.ByteBuffer;
+import java.util.List;
+
+public class ByteArrayKeyValueScheme extends RawScheme implements KeyValueScheme {
+    @Override
+    public List<Object> deserializeKeyAndValue(ByteBuffer key, ByteBuffer value) {
+        if (key == null) {
+            return deserialize(value);
+        }
+        Object keyTuple = deserialize(key).get(0);
+        Object valueTuple = deserialize(value).get(0);
+
+        return new Values(ImmutableMap.of(keyTuple, valueTuple));
+    }
+}
